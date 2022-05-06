c881f26f3f5feb0ffb2035b2ba7be78f12845537
Gerry5126 <53771072+Gerry5126@users.noreply.github.com>
2022-05-06 21:41:36 +0800

    Remove TODO

--- src/main/java/xyz/garslity093/realmcore/PluginListeners.java
+++ src/main/java/xyz/garslity093/realmcore/PluginListeners.java
@@ -2,6 +2,4 @@ package xyz.garslity093.realmcore;
 
 import org.bukkit.event.Listener;
 
-public final class PluginListeners implements Listener {
-    // TODO add something
-}
+public final class PluginListeners implements Listener {}
